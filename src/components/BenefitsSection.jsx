e7ab5ce14136a21b94aa2481485c631b9d52d680
Andres Ortiz Osorio <ao2005ortiz@gmail.com>
2025-05-04 17:26:43 -0500

    fixedimg

--- src/components/BenefitsSection.jsx
+++ src/components/BenefitsSection.jsx
@@ -4,15 +4,15 @@ export default function BenefitsSection() {
   return (
     <>
       <section className="mx-8 mb-24 md:col-start-1 md:col-end-5 lg:col-end-9 xl:col-end-12 xl:col-start-2">
-        <div className="lg:grid lg:grid-cols-2 lg:gap-12">
-          <div className="hidden lg:block ">
+        <div className="grid grid-cols-1 gap-12 items-start lg:grid-cols-2">
+          <div className="hidden lg:flex lg:justify-center">
             <img
               src="./src/assets/smileguy.jpg"
-              className="rounded-xl"
+              className="rounded-xl object-cover max-w-[365px] max-h-[530px]"
               alt=""
             />
           </div>
-          <div className="">
+          <div className="min-w-[365px] ">
             <h1 className="text-4xl font-bold mb-4">Benefits</h1>
             <p className="mb-8 max-w-full text-xl">
               Designed to help you take control of your finances and achieve
@@ -20,7 +20,7 @@ export default function BenefitsSection() {
               complete overview of your finances, allowing you to manage your
               money with confidence.
             </p>
-            <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-8 lg:gap-12">
               <BenefitBlock>Accessible and Convenient</BenefitBlock>
               <BenefitBlock>Personalized Suggestions</BenefitBlock>
               <BenefitBlock>Improved Financial Management</BenefitBlock>
